ac09754d9c08083ee611f9e68d98b0b58d06b1e5
duchoangkim123 <duc42165@huce.edu.vn>
2025-04-07 15:32:35 +0700

    12. Hiểu rõ cách triển khai Dark Mode & Light Mode trong MUI

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,32 @@ import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+//khi làm next.js hay nhưng framework dùng ssr thì để ngăn chặn dark-mode flickering phải dùng import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
+//Link đọc: https://v5.mui.com/material-ui/experimental-api/css-theme-variables/migration/
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Hello</div>
       <Button variant="text">Text</Button>
-      <Button variant="contained" >Contained</Button>
+      <Button variant="contained" color="success">Contained</Button>
       <Button variant="outlined">Outlined</Button>
 
       <br />
