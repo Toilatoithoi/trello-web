78c16381f4fe0293d003b59cf4b83e12365b4628
duchoangkim123 <duc42165@huce.edu.vn>
2025-04-23 11:55:35 +0700

    19. Tối ưu cấu trúc thư mục dự án chuyên nghiệp
    Refactor Project Structure.

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container'
+import AppBar from '../../components/AddBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+function Board() {
+  return (
+    <>
+      <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+        <AppBar />
+        <BoardBar />
+        <BoardContent />
+      </ Container>
+    </>
+  )
+}
+
+export default Board
